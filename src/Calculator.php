b6915a3f4637d3fc9dcc8301c99a46803a2db583
Jonathan Zarate <jonathan.zarate@dinkbit.com>
2019-03-25 13:02:11 -0600

    Initial commit

---
+++ src/Calculator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Itch;
+
+use InvalidArgumentException;
+
+class Calculator
+{
+    public function sum($a, $b)
+    {
+        if (!is_numeric($a) || !is_numeric($b)) {
+            throw new InvalidArgumentException('Número invalido');
+        }
+
+        return $a + $b;
+    }
+}
