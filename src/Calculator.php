c3218c40564c5519d567c68f77a45946508d8b13
Jonathan Zarate <jonathan.zarate@dinkbit.com>
2019-03-27 18:30:25 -0600

    Added InvalidArgumentException

--- src/Calculator.php
+++ src/Calculator.php
@@ -2,10 +2,16 @@
 
 namespace Itch;
 
+use InvalidArgumentException;
+
 class Calculator
 {
     public function sum($a, $b)
     {
+        if (!is_numeric($a) || !is_numeric($b)) {
+            throw new InvalidArgumentException('Número invalido');
+        }
+
         return $a + $b;
     }
 }
