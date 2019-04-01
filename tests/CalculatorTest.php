5178337936e40898129f16f5f7d297c03623da8b
Jonathan <zaratedev@gmail.com>
2019-03-31 21:01:33 -0600

    Added test

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -5,5 +5,11 @@ use Itch\Calculator;
 class CalculatorTest extends PHPUnit\Framework\TestCase
 {
 
-    //
+    /** @test */
+    public function it_should_return_the_sum_of_two_number_when_the_parameters_are_correct()
+    {
+        $result = (new Calculator())->sum(2, 2);
+        
+        $this->assertEquals(4, $result);
+    }
 }
