200063774c8b1a97bddd397f33e787a3be7747f1
Jonathan <zaratedev@gmail.com>
2019-03-31 20:25:10 -0600

    Refactor

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -5,29 +5,5 @@ use Itch\Calculator;
 class CalculatorTest extends PHPUnit\Framework\TestCase
 {
 
-    /** @test */
-    public function it_should_return_an_error_when_one_or_two_are_missing()
-    {
-        try {
-            (new Calculator())->sum('four', 2);
-        } catch (\InvalidArgumentException $e) {
-            $this->assertSame('Número invalido', $e->getMessage());
-        }
-    }
-
-    /** @test */
-    public function it_should_return_the_sum_of_two_number_when_the_parameters_are_correct()
-    {
-        $result = (new Calculator())->sum(2, 2);
-
-        $this->assertEquals(4, $result);
-    }
-
-    /** @test */
-    public function it_should_return_the_sum_of_the_numbers_even_if_the_numbers_are_string()
-    {
-        $result = (new Calculator())->sum('2', '2');
-        
-        $this->assertEquals(4, $result);
-    }
+    //
 }
