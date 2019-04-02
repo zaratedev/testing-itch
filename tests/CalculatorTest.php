eab5c1efdc111e4cbca137a667997a73a4d6bfb8
Jonathan Zarate <jonathan.zarate@dinkbit.com>
2019-04-02 15:15:24 -0600

    Other test

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -12,4 +12,12 @@ class CalculatorTest extends PHPUnit\Framework\TestCase
         
         $this->assertEquals(4, $result);
     }
+
+    /** @test */
+    public function it_should_return_the_sum_of_the_numbers_even_if_the_numbers_are_string()
+    {
+        $result = (new Calculator())->sum('2', '2');
+        
+        $this->assertEquals(4, $result);
+    }
 }
